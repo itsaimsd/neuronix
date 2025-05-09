c529dbb2abffcafa8d326d86fa7b5e87860b1d04
itsaimsd <madhusudantiwari2003@gmail.com>
2025-05-09 12:59:13 +0530

    deleting portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/services" element={<Services />} />
-            <Route path="/portfolio" element={<Portfolio />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
